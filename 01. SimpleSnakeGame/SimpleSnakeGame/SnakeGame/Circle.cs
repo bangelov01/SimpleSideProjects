cc8755c9b927cc6c9c946aaa80b8b24d3436f2a4
Bogdan Angelov <bgd.angelov@gmail.com>
2020-12-23 15:04:00 +0200

    Add Snake game project

---
+++ 01. SimpleSnakeGame/SimpleSnakeGame/SnakeGame/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    class Circle
+    {
+        public Circle()
+        {
+            X = 0;
+            Y = 0;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
